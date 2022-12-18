1264f8b969478a2651c8491d8d7df4fa854a1b17
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2022-12-17 21:28:34 -0600

    Setup fast refresh

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -4,24 +4,5 @@ export default {
     public: '/',
     src: '/dist',
   },
-  plugins: [
-    /* ... */
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+  plugins: ['@snowpack/plugin-react-refresh']
 };
